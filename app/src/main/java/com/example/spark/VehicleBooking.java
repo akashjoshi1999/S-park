0c0f54482961cde1a357daa611e4488c49f04f61
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-02-29 00:44:29 +0530

    Initial commit

--- app/src/main/java/com/example/spark/VehicleBooking.java
+++ app/src/main/java/com/example/spark/VehicleBooking.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class VehicleBooking extends AppCompatActivity {
 
     private TextView textDirection,textBoooking;
+    private ImageView imageView1,imageView2,imageView3;
+    private TextView textViewimg1,TextViewimg2,TextViewimg3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,13 @@ public class VehicleBooking extends AppCompatActivity {
 
         textDirection = (TextView)findViewById(R.id.textDirection);
         textBoooking = (TextView)findViewById(R.id.textBooking);
+        imageView1 = (ImageView) findViewById(R.id.image1);
+        imageView2 = (ImageView) findViewById(R.id.image2);
+        imageView3 = (ImageView) findViewById(R.id.image3);
 
+        textViewimg1 = (TextView) findViewById(R.id.img1);
+        TextViewimg2 = (TextView) findViewById(R.id.img2);
+        TextViewimg3 = (TextView) findViewById(R.id.img3);
         textDirection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
