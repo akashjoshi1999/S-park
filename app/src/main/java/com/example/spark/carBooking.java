563d5a39f025bb47c7f6a3a39f061499de7b0f56
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-02-28 23:56:36 +0530

    Initial commit

--- app/src/main/java/com/example/spark/carBooking.java
+++ app/src/main/java/com/example/spark/carBooking.java
@@ -1,10 +1,18 @@
 package com.example.spark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,14 +21,29 @@ public class carBooking extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private List<BookTheVehicle> bookTheVehicles;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_booking);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("AccountDetails");
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        bookTheVehicles = new ArrayList<>()
+        bookTheVehicles = new ArrayList<BookTheVehicle>();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot dataSnapshot1: dataSnapshot.getChildren()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(carBooking.this,"Something Wrong",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
