f53c9b182eff9e258b136a24d4513dc0f9176a1e
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-03-12 11:55:32 +0530

    Initial commit

--- app/src/main/java/com/example/spark/carBookingBytime.java
+++ app/src/main/java/com/example/spark/carBookingBytime.java
@@ -11,14 +11,17 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class carBookingBytime extends AppCompatActivity {
 
     private TextView textViewStartTime,textViewEndTime,TextViewStartDate,TextViewEndDate,textViewFinalBook;
     DatePickerDialog.OnDateSetListener setListener;
     TimePickerDialog.OnTimeSetListener onTimeSetListener;
-    public String date,date2;
+    public String date1,date2,time1,time2;
     String AmPm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,7 @@ public class carBookingBytime extends AppCompatActivity {
                         } else {
                             AmPm = "AM";
                         }
+                        time1 = "%2d:%2d"+hourOfDay+minutes + AmPm;
                         textViewStartTime.setText(String.format("%2d:%2d",hourOfDay,minutes)+AmPm);
                     }
                 },hour,minute,false);
@@ -69,6 +73,7 @@ public class carBookingBytime extends AppCompatActivity {
                         } else {
                             AmPm = "AM";
                         }
+                        time2 = "%2d:%2d"+hourOfDay+minutes+AmPm;
                         textViewEndTime.setText(String.format("%2d:%2d",hourOfDay,minutes)+AmPm);
                     }
                 },hour,minute,false);
@@ -84,8 +89,8 @@ public class carBookingBytime extends AppCompatActivity {
                     @Override
                     public void onDateSet(DatePicker view, int year, int month, int day) {
                         month = month+1;
-                        date= day+"/"+ month +"/" +year;
-                        TextViewStartDate.setText(date);
+                        date1= day+"/"+ month +"/" +year;
+                        TextViewStartDate.setText(date1);
                     }
                 },year,month,day);
                 datePickerDialog.show();
@@ -111,7 +116,31 @@ public class carBookingBytime extends AppCompatActivity {
         textViewFinalBook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                try {
 
+                    String format = "MM/dd/yyyy hh:mm a";
+
+                    SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+                    Date dateObj1 = sdf.parse(date1 + " " + time1);
+                    Date dateObj2 = sdf.parse(date2 + " " + time2);
+                    System.out.println(dateObj1);
+                    System.out.println(dateObj2 + "\n");
+
+                    DecimalFormat crunchifyFormatter = new DecimalFormat("###,###");
+
+                    // getTime() returns the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this Date object
+                    long diff = dateObj2.getTime() - dateObj1.getTime();
+
+                    int diffDays = (int) (diff / (24 * 60 * 60 * 1000));
+                    int diffhours = (int) (diff / (60 * 60 * 1000));
+                    String bookHourDiff = crunchifyFormatter.format(diffhours);
+                    int diffmin = (int) (diff / (60 * 1000));
+                    String bookMinuteDiff = crunchifyFormatter.format(diffmin);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
 
