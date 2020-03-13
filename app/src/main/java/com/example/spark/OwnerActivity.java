23051ea000b17f6a5c5efe4267fbbdfb2082aba9
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-03-13 15:25:24 +0530

    Initial commit

--- app/src/main/java/com/example/spark/OwnerActivity.java
+++ app/src/main/java/com/example/spark/OwnerActivity.java
@@ -2,13 +2,21 @@ package com.example.spark;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -20,42 +28,58 @@ import com.google.firebase.storage.FirebaseStorage;
 
 public class OwnerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    private RecyclerView recyclerView;
     FirebaseAuth firebaseAuth;
-    TextView car;
-    FirebaseDatabase firebaseDatabase;
-    String carPark;
+    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner);
-        car = (TextView) findViewById(R.id.textCar);
-
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("AccountDetails")
-                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                .child("car_standing");
-
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                carPark = dataSnapshot.child("car_standing").getValue(String.class);
-                car.setText(carPark);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+        databaseReference = FirebaseDatabase.getInstance().getReference("AccountDetails").child("QGVsYAYdfiQQ1Fu6vW3CfdBxSlA3");
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<ParkingSpot> options =
+                new FirebaseRecyclerOptions.Builder<ParkingSpot>()
+                        .setQuery(databaseReference, ParkingSpot.class)
+                        .build();
+
+        FirebaseRecyclerAdapter<ParkingSpot,OwnerHolder> adapter =
+                new FirebaseRecyclerAdapter<ParkingSpot, OwnerHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull OwnerHolder holder, int position, @NonNull ParkingSpot model) {
+                        String parkingSpot = model.getCar_standing();
+                        if(parkingSpot.equals("Yes")){
+                            holder.spot.setBackgroundColor(Color.parseColor("#ff0000"));
+                        } else {
+                            holder.spot.setBackgroundColor(Color.parseColor("#00ff00"));
+                        }
+                    }
+
+                    @NonNull
+                    @Override
+                    public OwnerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_owner, parent, false);
+                        OwnerHolder holder = new OwnerHolder(view);
+                        return holder;
+                    }
+                };
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
         switch (id) {
-            case R.id.nav_account:
-                startActivity(new Intent(getApplicationContext(), AccountActivity.class));
-                finish();
-                break;
             case R.id.nav_history:
                 startActivity(new Intent(getApplicationContext(), HistoryOwnerActivity.class));
                 finish();
