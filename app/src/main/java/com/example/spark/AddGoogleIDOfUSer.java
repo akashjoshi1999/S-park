f3fae0b1d4cbbd92f62a26710373635d73e4f3d3
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-04-10 13:01:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/spark/AddGoogleIDOfUSer.java
@@ -0,0 +1,13 @@
+package com.example.spark;
+
+public class AddGoogleIDOfUSer {
+    public String Id;
+
+    public AddGoogleIDOfUSer(){
+
+    }
+
+    public AddGoogleIDOfUSer(String id) {
+        Id = id;
+    }
+}
--- app/src/main/java/com/example/spark/AddGooglePayActivity.java
+++ app/src/main/java/com/example/spark/AddGooglePayActivity.java
@@ -1,14 +1,63 @@
 package com.example.spark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddGooglePayActivity extends AppCompatActivity {
 
-    @Override
+    public TextView textViewButtonID;
+    public EditText editTextAddID;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_google_pay);
+
+        editTextAddID = findViewById(R.id.editTextAddGoogleID);
+        textViewButtonID = findViewById(R.id.textViewButtonAddGoogleID);
+
+        textViewButtonID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addGoogleID();
+            }
+        });
+
+
+
+
+    }
+
+    private void addGoogleID() {
+        final String AddID = editTextAddID.getText().toString().trim();
+        if (TextUtils.isEmpty(AddID)) {
+            editTextAddID.setError("ID required");
+            return;
+        }
+        AddGoogleIDOfUSer addGoogleIDOfUSer = new AddGoogleIDOfUSer(
+            AddID
+        );
+        FirebaseDatabase.getInstance().getReference("AccountDetails")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .setValue(addGoogleIDOfUSer).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(AddGooglePayActivity.this,"ID Stored",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
