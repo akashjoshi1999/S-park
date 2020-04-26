54d75f1671f43c81eeb6713866fffa7a035f2844
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-04-26 10:10:07 +0530

    Initial commit

--- app/src/main/java/com/example/spark/PaymentActivity.java
+++ app/src/main/java/com/example/spark/PaymentActivity.java
@@ -1,10 +1,14 @@
 package com.example.spark;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +26,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicMarkableReference;
 
@@ -36,6 +41,8 @@ public class PaymentActivity extends AppCompatActivity {
     private int accept,accept_1;
     private String PaymentGooglePayID,PaymentName,GOOGLE_PAY_PACKAGE_NAME,OwnerName;
     private int GOOGLE_PAY_REQUEST_CODE;
+    final int UPI_PAYMENT = 0;
+    public int Amount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,13 +52,14 @@ public class PaymentActivity extends AppCompatActivity {
         GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user";
         GOOGLE_PAY_REQUEST_CODE = 123;
 
+
         editTextOwnerName = (EditText) findViewById(R.id.editTextOwnerName);
         editTextTotalAmount = (EditText) findViewById(R.id.editTextAmount);
         textViewPayment = (TextView) findViewById(R.id.textViewPaymentButton);
         PaymentName = "Parking Spot";
 
         Bundle bundle = getIntent().getExtras();
-        final int Amount= bundle.getInt("Amount");
+        Amount= bundle.getInt("Amount");
         progressDialog = new ProgressDialog(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
         final DatabaseReference databaseReference = firebaseDatabase.getReference("data").child("QGVsYAYdfiQQ1Fu6vW3CfdBxSlA3");
@@ -108,6 +116,95 @@ public class PaymentActivity extends AppCompatActivity {
                 Log.v("abc","end");
                 // PaymentGooglePayID owner gid
                 // OwnerName owner name
+
+            }
+
+            private void PayUsingUPI(String name, String upid, String note, String amount) {
+                Uri uri =
+                        new Uri.Builder()
+                                .scheme("upi")
+                                .authority("pay")
+                                .appendQueryParameter("pa", upid)
+                                .appendQueryParameter("pn", name)
+//                                .appendQueryParameter("mc", "your-merchant-code")
+//                                .appendQueryParameter("tr", "your-transaction-ref-id")
+                                .appendQueryParameter("tn", note)
+                                .appendQueryParameter("am", amount)
+                                .appendQueryParameter("cu", "INR")
+//                                .appendQueryParameter("url", "your-transaction-url")
+                                .build();
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(uri);
+                Intent chooser = Intent.createChooser(intent,"Pay with");
+
+                if(chooser.resolveActivity(getPackageManager()) != null){
+                    startActivityForResult(chooser,UPI_PAYMENT);
+                } else {
+                    Toast.makeText(PaymentActivity.this,"No UPI app found, please install one to continue",Toast.LENGTH_SHORT).show();
+                }
+
+//                intent.setPackage(GOOGLE_PAY_PACKAGE_NAME);
+//                startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE);
+            }
+
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case UPI_PAYMENT:
+                if ((RESULT_OK == resultCode) || (resultCode == 11)) {
+                    if (data != null) {
+                        String trxt = data.getStringExtra("response");
+                        Log.e("UPI", "onActivityResult: " + trxt);
+                        ArrayList<String> dataList = new ArrayList<>();
+                        dataList.add(trxt);
+                        upiPaymentDataOperation(dataList);
+                    } else {
+                        Log.e("UPI", "onActivityResult: " + "Return data is null");
+                        ArrayList<String> dataList = new ArrayList<>();
+                        dataList.add("nothing");
+                        upiPaymentDataOperation(dataList);
+                    }
+                } else {
+                    //when user simply back without payment
+                    Log.e("UPI", "onActivityResult: " + "Return data is null");
+                    ArrayList<String> dataList = new ArrayList<>();
+                    dataList.add("nothing");
+                    upiPaymentDataOperation(dataList);
+                }
+                break;
+        }
+
+    }
+
+    private void upiPaymentDataOperation(ArrayList<String> data) {
+        if (isConnectionAvailable(PaymentActivity.this)) {
+            String str = data.get(0);
+            Log.e("UPIPAY", "upiPaymentDataOperation: "+str);
+            String paymentCancel = "";
+            if(str == null) str = "discard";
+            String status = "";
+            String approvalRefNo = "";
+            String response[] = str.split("&");
+            for (int i = 0; i < response.length; i++) {
+                String equalStr[] = response[i].split("=");
+                if(equalStr.length >= 2) {
+                    if (equalStr[0].toLowerCase().equals("Status".toLowerCase())) {
+                        status = equalStr[1].toLowerCase();
+                    }
+                    else if (equalStr[0].toLowerCase().equals("ApprovalRefNo".toLowerCase()) || equalStr[0].toLowerCase().equals("txnRef".toLowerCase())) {
+                        approvalRefNo = equalStr[1];
+                    }
+                }
+                else {
+                    paymentCancel = "Payment cancelled by user.";
+                }
+            }
+            if (status.equals("success")) {
+                //Code to handle successful transaction here.
                 UserPayment userPayment = new UserPayment(
                         OwnerName,PaymentGooglePayID,Amount
                 );
@@ -137,33 +234,37 @@ public class PaymentActivity extends AppCompatActivity {
                     }
                 });
                 if(accept == accept_1){
-                    Toast.makeText(PaymentActivity.this, "Payment Successful", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(PaymentActivity.this, "Transaction successful.", Toast.LENGTH_SHORT).show();
+                    Log.e("UPI", "payment successful: "+approvalRefNo);
                     progressDialog.dismiss();
                     Intent intent = new Intent(PaymentActivity.this, carBooking.class);
                     startActivity(intent);
                 }
             }
-
-            private void PayUsingUPI(String name, String upid, String note, String amount) {
-                Uri uri =
-                        new Uri.Builder()
-                                .scheme("upi")
-                                .authority("pay")
-                                .appendQueryParameter("pa", upid)
-                                .appendQueryParameter("pn", name)
-//                                .appendQueryParameter("mc", "your-merchant-code")
-//                                .appendQueryParameter("tr", "your-transaction-ref-id")
-                                .appendQueryParameter("tn", note)
-                                .appendQueryParameter("am", amount)
-                                .appendQueryParameter("cu", "INR")
-//                                .appendQueryParameter("url", "your-transaction-url")
-                                .build();
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(uri);
-                intent.setPackage(GOOGLE_PAY_PACKAGE_NAME);
-                startActivityForResult(intent, GOOGLE_PAY_REQUEST_CODE);
+            else if("Payment cancelled by user.".equals(paymentCancel)) {
+                Toast.makeText(PaymentActivity.this, "Payment cancelled by user.", Toast.LENGTH_SHORT).show();
+                Log.e("UPI", "Cancelled by user: "+approvalRefNo);
             }
-        });
+            else {
+                Toast.makeText(PaymentActivity.this, "Transaction failed.Please try again", Toast.LENGTH_SHORT).show();
+                Log.e("UPI", "failed payment: "+approvalRefNo);
+            }
+        } else {
+            Log.e("UPI", "Internet issue: ");
+            Toast.makeText(PaymentActivity.this, "Internet connection is not available. Please check and try again", Toast.LENGTH_SHORT).show();
+        }
     }
 
+    public static boolean isConnectionAvailable(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+            if (netInfo != null && netInfo.isConnected()
+                    && netInfo.isConnectedOrConnecting()
+                    && netInfo.isAvailable()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
