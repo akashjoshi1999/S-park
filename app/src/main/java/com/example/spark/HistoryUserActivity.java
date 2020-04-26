2b383c954a1b74a89e28876813a7c8bb34155aa3
Aakash Joshi <38850663+akashjoshi1999@users.noreply.github.com>
2020-04-26 21:50:22 +0530

    Initial commit

--- app/src/main/java/com/example/spark/HistoryUserActivity.java
+++ app/src/main/java/com/example/spark/HistoryUserActivity.java
@@ -35,19 +35,18 @@ public class HistoryUserActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         list = new ArrayList<PaymentUser>();
 
-        databaseReference = (DatabaseReference) FirebaseDatabase.getInstance().getReference("AccountDetails").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+        databaseReference = (DatabaseReference) FirebaseDatabase.getInstance().getReference("AccountDetails")
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("history")
                 .addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.hasChild("history")) {
-                    for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-                        PaymentUser p = dataSnapshot1.child("history").getValue(PaymentUser.class);
-                        list.add(p);
-                    }
-                    myAdapterForUser = new MyAdapterForUser(HistoryUserActivity.this, list);
-                } else {
-                    Toast.makeText(HistoryUserActivity.this,"you don`t have any transaction",Toast.LENGTH_LONG).show();
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    String key = dataSnapshot1.getKey();
+                    PaymentUser p = dataSnapshot1.child(key).getValue(PaymentUser.class);
+                    list.add(p);
                 }
+                myAdapterForUser = new MyAdapterForUser(HistoryUserActivity.this, list);
+
             }
 
             @Override
